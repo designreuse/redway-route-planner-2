bbdd81ee9876b67b5b7a214eb3ae08f155bb8047
Louis Houghton <louis@louishoughton.com>
2016-12-30 11:13:07 +0000

    Add basic Spring boot app

---
+++ src/main/java/org/mkhackathon/App.java
@@ -0,0 +1,20 @@
+package org.mkhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello!";
+    }
+}
