d2755da08025fc232506bfdcd8ee6465fd8e0c0a
Louis Houghton <louis@louishoughton.com>
2017-01-11 21:37:21 +0000

    Add global exception handler

--- src/main/java/org/mkhackathon/routing/GlobalExceptionHandler.java
+++ src/main/java/org/mkhackathon/routing/GlobalExceptionHandler.java
@@ -1,12 +1,21 @@
 package org.mkhackathon.routing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
-    @ExceptionHandler
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
     public String handler(Exception e) {
-
+        LOG.error("Caught exception", e);
+        return e.getMessage();
     }
 }
