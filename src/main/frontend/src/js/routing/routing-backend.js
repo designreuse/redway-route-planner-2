dd7d33af2dffb482e5cda0dfa2c8b95c0de798ca
Louis Houghton <louis@louishoughton.com>
2017-03-05 21:33:16 +0000

    If in phonegap, use deployed server

--- src/main/frontend/src/js/routing/routing-backend.js
+++ src/main/frontend/src/js/routing/routing-backend.js
@@ -2,15 +2,17 @@
 
     function routingBackend($http) {
 
+        const server = window.location.origin.split(":")[0] === "file" ? "http://rrp2.mkhackathon.org" : "";
+
         function search(start, end) {
-            return $http.post("/routing", {
+            return $http.post(server + "/routing", {
                 start: start,
                 end: end
             });
         }
 
         function getPlaces() {
-            return $http.get("/places");
+            return $http.get(server + "/places");
         }
 
         return {
